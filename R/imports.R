2196a349d69787dd45cc59f7d62bd4d5070259ec
James Blair <james.m.blair09@gmail.com>
2017-12-29 23:47:44 -0700

    Add imports.R to import functions from other packages

---
+++ R/imports.R
@@ -0,0 +1,21 @@
+# Functions imported from other packages
+
+#' Pipe operator
+#'
+#' @name %>%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @importFrom magrittr %>%
+#' @usage lhs \%>\% rhs
+NULL
+
+#' Tibble function
+#'
+#' @name tibble
+#' @rdname tibble
+#' @keywords internal
+#' @export
+#' @importFrom tibble tibble
+#' @usage tibble()
+NULL
