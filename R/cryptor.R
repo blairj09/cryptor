5d0cc08d2337eb7a674fd8829ce688213c5df39d
James Blair <james.m.blair09@gmail.com>
2017-12-29 17:00:44 -0700

    Update package level documentation

--- R/cryptor.R
+++ R/cryptor.R
@@ -1,12 +1,10 @@
 #' cryptor: An R wrapper for the \url{https://www.cryptocompare.com} API.
-#' @references
 #'
 #' This package provides access to the crypto currency API documented at
 #' \url{https://www.cryptocompare.com/api}. The API is currently public and can
 #' be accessed without authentication or credentials. The API authors have enforced
-#' call hourly and second limits which can be accessed at
-#' \url{https://min-api.cryptocompare.com/stats/rate/hour/limit} and
-#' \url{https://min-api.cryptocompare.com/stats/rate/second/limit} respectively.
+#' call \href{https://min-api.cryptocompare.com/stats/rate/hour/limit}{hourly} and
+#' \href{https://min-api.cryptocompare.com/stats/rate/second/limit}{second} limits.
 #'
 #' @section Functions:
 #'
