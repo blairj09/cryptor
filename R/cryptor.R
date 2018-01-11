bddd18213ea189b789e4e653c536f951c2936e3e
James Blair <james.m.blair09@gmail.com>
2018-01-11 15:36:49 -0700

    Small update to package documentation

--- R/cryptor.R
+++ R/cryptor.R
@@ -3,8 +3,9 @@
 #' This package provides access to the crypto currency API documented at
 #' \url{https://www.cryptocompare.com/api}. The API is currently public and can
 #' be accessed without authentication or credentials. The API authors have enforced
-#' call \href{https://min-api.cryptocompare.com/stats/rate/hour/limit}{hourly} and
-#' \href{https://min-api.cryptocompare.com/stats/rate/second/limit}{second} limits.
+#' \href{https://min-api.cryptocompare.com/stats/rate/hour/limit}{hourly} and
+#' \href{https://min-api.cryptocompare.com/stats/rate/second/limit}{second} call
+#' limits.
 #'
 #' @section Functions:
 #' \itemize{
